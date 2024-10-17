022bbc18f4e3e189eb2eb32ab57652f6702892e0
Oleksandr Zaiets <mykola.oleksandr.zaiets@gmail.com>
2024-10-17 18:02:39 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,33 +1,33 @@
 from app.main import get_coin_combination
 
 
-def test_get_coin_combination_zero():
+def test_get_coin_combination_zero() -> None:
     assert get_coin_combination(0) == [0, 0, 0, 0]
 
 
-def test_get_coin_combination_penny():
+def test_get_coin_combination_penny() -> None:
     assert get_coin_combination(1) == [1, 0, 0, 0]
 
 
-def test_get_coin_combination_nickel():
+def test_get_coin_combination_nickel() -> None:
     assert get_coin_combination(5) == [0, 1, 0, 0]
 
 
-def test_get_coin_combination_dime():
+def test_get_coin_combination_dime() -> None:
     assert get_coin_combination(10) == [0, 0, 1, 0]
 
 
-def test_get_coin_combination_quarter():
+def test_get_coin_combination_quarter() -> None:
     assert get_coin_combination(25) == [0, 0, 0, 1]
 
 
-def test_get_coin_combination_mixed_41():
+def test_get_coin_combination_mixed_41() -> None:
     assert get_coin_combination(41) == [1, 1, 1, 1]
 
 
-def test_get_coin_combination_mixed_99():
+def test_get_coin_combination_mixed_99() -> None:
     assert get_coin_combination(99) == [4, 0, 2, 3]
 
 
-def test_get_coin_combination_large_value():
+def test_get_coin_combination_large_value() -> None:
     assert get_coin_combination(100) == [0, 0, 0, 4]
